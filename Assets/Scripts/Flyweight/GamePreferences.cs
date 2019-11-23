8e82b6cd22ad56cf598d5b6e3e98a184bba07245
batraz.dzhioev <baticpro@gmail.com>
2019-11-23 09:47:32 +0300

    add ScriptableObject

---
+++ Assets/Scripts/Flyweight/GamePreferences.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Flyweight
+{
+    [CreateAssetMenu(menuName = "Game Preferences")]
+    public class GamePreferences : ScriptableObject
+    {
+        public float TimeSpeed;
+        public int SpawnItems;
+    }
+}
